679ab49026522d4008862b27a57f0016d3efb239
brettmccausland <brettmccausland@yahoo.com>
2019-02-10 23:37:06 -0800

    Query class in progress

--- assn2.py
+++ assn2.py
@@ -42,6 +42,34 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from bs4  import BeautifulSoup
 ps = PorterStemmer()
 
+class Query:
+    def __init__(self,str_words,stopwords):
+     self.rank=0
+     self.dict_words={}
+     self.numwords=0
+     self.tf_wieghts=[]
+     
+     words = ps.stem(str_words).lower().strip().split(' ')
+     for w in words:
+        if w not in stopwords:
+            if w in self.dict_words:
+              self.dict_words[w]+= 1
+              self.numwords+=1
+            else:
+              self.dict_words[w] = 1
+              self.numwords+=1
+     #self.vectorSpace()
+     for word in self.dict_words:
+             self.tf_wieghts.append(self.dict_words[word]/self.numwords)
+              
+     def getTF(self,word):
+         return self.dict_words[word]/self.numwords
+     
+     def vectorSpace(self):
+         for word in self.dict_words:
+             self.tf_wieghts.append(self.getTF(word))
+   
+  
 class term:
     def __init__(self,numdocs,postings):
         self.numdocs=numdocs
@@ -58,7 +86,28 @@ class term:
       if fname in self.postings:
           return True
       return False
-
+  
+    def printPostings(self):
+        print(self.postings)
+        
+    def getPostings(self):
+        return self.postings
+    
+    def get_Doc_tf(self,doc,dict_docs):
+        if fname in self.postings:
+         postnum=self.postings[doc]
+         num_word_in_doc=dict_docs[doc]
+         return postnum/num_word_in_doc
+        else:
+         return 0
+     
+    def get_IDF_TF(self,doc,dict_docs):
+          tot_numdocs=len(dict_docs)
+          numdoc_term_in= len(self.postings)
+          df= tot_numdocs/numdoc_term_in+1
+          idf = math.log(2, df)
+          return self.get_Doc_tf(doc,dict_docs) * idf
+        
     def display_wieghtings(self,dict_docs):
        for post in self.postings:
           #print(post)
@@ -66,7 +115,7 @@ class term:
           #print( numwords_in_post)
           term_freq = self.postings[post]
           #print (term_freq)
-          tf= term_freq/numwords_in_post
+          tf = term_freq/numwords_in_post
           numdicts=len(dict_docs)
           #print(numdicts)
           numdoc_term_in= len(self.postings)
@@ -99,8 +148,6 @@ def load_line(l_line, stopwords):
 # Updates word count in fname for every word
 #
 def line_proc(line, dict_terms,dict_docs,fname,stopwords):
-   print("-----------------------------------------------------------")
-   print(line)
    for word in line:
        w=word.lower()
        w=ps.stem(w)
@@ -122,11 +169,11 @@ def loadInvertedIndex(dict_terms,dict_docs,path,filetype,stopwords):
       if(filetype=="trec"):
          print(' trec ')
          soup = BeautifulSoup(open(path),"lxml")
-         print('Good')
          #print(soup)
          for doc in soup.find_all('doc'):
             for eachdoc in doc.find_all('docno'):
-                filename=eachdoc.text
+                s=eachdoc.text.strip().split(' ')
+                filename=s[0]
                 dict_docs[filename] = 0
                 for txt in doc.find('text'):
                   line_proc(txt.strip().split(' '),dict_terms,dict_docs, filename,stopwords)
@@ -151,8 +198,46 @@ def Prompt(dict_terms,dict_docs):
    else:
        print('not in the inverted file')
 #Part 1 -
-#def cosineSimilarity(query, document):
+def cosineSimilarity(arr_query, arr_doc):
+	dot_product = np.dot(arr_query,arr_doc)
+	norm_query = np.linalg.norm(arr_query)
+	norm_doc = np.linalg.norm(arr_doc)
+	return dot_product / (norm_query * norm_doc )
 
+"""def preprocessQuery(str_Query,stopwords):
+    w=[]
+    l=[]
+    words = ps.stem(str_Query).lower().strip().split(' ')
+    for w in words:
+        if w not in stopwords:
+            l.append(w)
+    return l
+"""           
+def documentRelavanceScore(fname,Query,dict_terms,stopwords,dict_docs):
+    # Use the vector space model to compute scores
+    # Use cosine Similarity to compute score between 
+    # Query and document
+    
+    #qi is the tf-idf weight of term i in the query
+    #di is the tf_idf term i in the document
+    d=[]
+    print(fname)
+    for word in Query.dict_words: #check every word in the query
+       print(word) 
+       if word in dict_terms: # if word is a dictionary word
+          print(word,"in dictionary")
+          curterm = dict_terms[word] # get term from dictionary
+          #curterm.printPostings()    
+          # curterm.get_Doc_tf(fname,dict_docs) # access the posting in the term for doc
+          d.append(curterm.get_IDF_TF(fname,dict_docs))
+          """ if curterm.existposting(fname):
+            print("match")
+            post = curterm.getPostings()
+            print(post)
+            print("match")"""
+       
+        
+    
 
 #Part 2 - Query Execution
 #run the queries in the file results file.txt
@@ -194,6 +279,7 @@ def DriverParserUpdate(stopwords,dict_terms,dict_docs):
 #  soup.get_text()
 
 
+
 stopwords=[]
 dict_terms={}
 dict_docs={}
@@ -203,4 +289,18 @@ path2="data/ap89_collection.html"
 
 filetype="trec"
 loadInvertedIndex(dict_terms,dict_docs,path2,filetype,stopwords)
+"""
 #DriverParserUpdate(stopwords,dict_terms,dict_docs)
+"""
+# ----------------------- driver documentRelavanceScore ------------------------
+
+fname= 'AP890101-0003'
+query_str = " A fast-spreading fire swept through a home "
+query=Query(query_str,stopwords)
+documentRelavanceScore(fname,query,dict_terms,stopwords,dict_docs)
+
+
+
+
+
+
